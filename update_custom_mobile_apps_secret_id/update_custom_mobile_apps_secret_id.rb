8a669b586f9390c591b72d12e8acb5f873017182
Lyndsey Ferguson <lyndsey.ferguson@appian.com>
2020-07-04 18:12:04 -0400

    Clean up and update role id
    This commit updates the vault network name to be something more sensible
    and it also refactors out the code to update GitHub secrets. This
    allowed me to update the role id when vault is destroyed recreated, and
    the role recreated

--- update_custom_mobile_apps_secret_id/update_custom_mobile_apps_secret_id.rb
+++ update_custom_mobile_apps_secret_id/update_custom_mobile_apps_secret_id.rb
@@ -1,30 +1,11 @@
 #!/usr/bin/env ruby
 
-require 'vault'
-require "rbnacl"
-require "base64"
-require 'octokit'
-
-def create_box(public_key)
-  b64_key = RbNaCl::PublicKey.new(Base64.decode64(public_key[:key]))
-  {
-    key_id: public_key[:key_id],
-    box: RbNaCl::Boxes::Sealed.from_public_key(b64_key)
-  }
-end
-
-# instruction from: https://developer.github.com/v3/actions/secrets/#get-a-repository-public-key
-github_token = File.read('update-public-repos-token.txt')
+require_relative 'update_github_secret_value'
 
 while true do
   secret = Vault.approle.create_secret_id("custom-mobile-apps-signer")
-  github_client = Octokit::Client.new(:access_token => github_token)
-  repo = github_client.repository('lyndsey-ferguson/article-example-securing-signing-assets')
-  box = create_box(github_client.get("#{repo.url}/actions/secrets/public-key"))
-  encrypted = box[:box].encrypt(secret.data[:secret_id])
-  response = github_client.put("#{repo.url}/actions/secrets/VAULT_CODESIGNING_SECRET_ID",
-    encrypted_value: Base64.strict_encode64(encrypted),
-    key_id: box[:key_id]
-  )
+  update_secret_value('VAULT_CODESIGNING_SECRET_ID', secret.data[:secret_id])
+  puts "Updated VAULT_CODESIGNING_SECRET_ID"
+  STDOUT.flush
   sleep 2700
 end
